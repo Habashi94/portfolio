00c9fb5d61c7c3755c8fad91f97392cbc0ca66bd
Mustafa Habashi <mustafahabashi@hotmail.com>
2020-06-29 18:07:27 +0100

    implemented a scroll section to make it more accessible

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./App.css";
 import NavBar from "./components/NavBar";
+import Main from "./components/Main";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <div className="App">
       <NavBar></NavBar>
+      <Main></Main>
+      <About></About>
+      <Contact></Contact>
     </div>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,50 @@
 import React from "react";
 import { Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-scroll";
 
 export default function NavBar() {
   return (
-    <div>
-      <Navbar bg="dark" variant="dark">
+    <div className="navbar">
+      <Navbar bg="dark" variant="dark" fixed="top">
         <Navbar.Brand href="#home">Navbar</Navbar.Brand>
         <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
+          {/* <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">About</Nav.Link>
+          <Nav.Link href="#pricing">Contact</Nav.Link> */}
+
+          <Link
+            activeClass="active"
+            to="main"
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={500}
+            id="navbar-items"
+          >
+            Main
+          </Link>
+          <Link
+            activeClass="active"
+            to="about"
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={500}
+            id="navbar-items"
+          >
+            About
+          </Link>
+          <Link
+            activeClass="active"
+            to="contact"
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={500}
+            id="navbar-items"
+          >
+            Contact
+          </Link>
         </Nav>
       </Navbar>
     </div>
