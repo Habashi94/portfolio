e5b4aa76ca05be1d08fda6d008bc1a29bfbf571a
Mustafa Habashi <mustafahabashi@hotmail.com>
2020-07-04 19:38:31 +0100

    adding welcome page to portfolio

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,19 @@
 import React from "react";
-
+import { Spring } from "react-spring/renderprops";
 export default function Main() {
-  return <div className="main-section" id="main"></div>;
+  return (
+    <div className="main-section" id="main">
+      <Spring
+        from={{ opacity: 0, marginTop: 0 }}
+        to={{ opacity: 1, marginTop: 100 }}
+        config={{ delay: 1000, duration: 1000 }}
+      >
+        {(props) => (
+          <div style={props}>
+            <h1>Welcome!</h1>
+          </div>
+        )}
+      </Spring>
+    </div>
+  );
 }
